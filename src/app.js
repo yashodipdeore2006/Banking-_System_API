895265a6c3283ab3a68f2aec4e467a5dec5bcbde
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-19 21:27:04 +0530

    Added dependencies cookiesParser and jsonWebToken

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
-import express from 'express'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+//==== Local Module ===
 import authRouter from './routes/auth.routes.js';
 
 
 //===================================
 const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
 
 
 //===================================
