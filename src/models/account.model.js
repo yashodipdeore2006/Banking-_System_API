47657af31e170bf24311e29e682f28fe8270827a
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-22 17:54:51 +0530

    fix : fixed typeO in user field and added type string in status

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const accountSchema = new mongoose.Schema({
   user: {
-    typeof: mongoose.Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: [true, 'Account must be associated with ta user'],
     index: true
   },
   status: {
+    type: String,
     enum: {
       values: ['ACTIVE', 'FROZEN', 'CLOSED'],
       message: 'Status can be either ACTIVE, FROZEN or CLOSED'
