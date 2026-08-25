abdc4e5734e7d4526eff8a1abfb0d4fd6b016194
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-25 21:52:20 +0530

    refactor "transactionRouter" url prefix from"/api/transactions" to "/api/transactions"

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use("/api/auth", authRouter);
 
 app.use("/api/accounts", accountRouter);
 
-app.use('/api/transaction', transactionRouter);
+app.use('/api/transactions', transactionRouter);
 
 //===================================
 export default app;
