ca0749a9ad8a575a2f172137c4056ad82e97acc3
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-27 21:48:05 +0530

    refactor(account): move getBalance method from ledger to account model

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import ledgerModel from "./ledger.model.js";
 
 const accountSchema = new mongoose.Schema({
   user: {
@@ -27,10 +28,62 @@ const accountSchema = new mongoose.Schema({
 accountSchema.index({ user: 1, status: 1 });
 
 
+/**
+ * ==== Calculate the total balance of ====
+ */
 
-//==================================
+accountSchema.methods.getBalance = async function () {
+  const result = await ledgerModel.aggregate([
+    {
+      $match: {
+        account: this._id
+      }
+    },
+
+    {
+      $group: {
+        _id: null,
+
+        credit: {
+          $sum: {
+            $cond: [
+              { $eq: ['$type', 'CREDIT'] },
+              '$amount',
+              0
+            ]
+          }
+        },
+
+        debit: {
+          $sum: {
+            $cond: [
+              { $eq: ['$type', 'DEBIT'] },
+              '$amount',
+              0
+            ]
+          }
+        }
+      }
+    },
+
+    {
+      $project: {
+        _id: 0,
+
+        totalBalance: {
+          $subtract: ['$credit', '$debit']
+        }
+      }
+    }
+  ]);
+
+  return result[0]?.totalBalance ?? 0;
+};
+
+
+//============================================================
 const accountModel = mongoose.model('Account', accountSchema);
 
 
-//==================================
+//=============================================================
 export default accountModel;
