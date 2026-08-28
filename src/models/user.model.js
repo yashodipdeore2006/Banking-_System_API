78614045c43f2e39e2f7eb7d04e19e5148d22278
Yashodip Deore <yashodipdeore2006@gmail.com>
2026-08-28 21:44:14 +0530

    feat(user): add systemUser field to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,6 +20,12 @@ const userSchema = mongoose.Schema({
     required: [true, "Password is required for creating an account"],
     minLength: [6, "Password should contain more than 6 characters"],
     select: false
+  },
+  systemUser: {
+    type: Boolean,
+    default: false,
+    immutable: true,
+    select: false
   }
 }, { timestamps: true });
 
